b5db03e1cf75875c58dbd61f5f970b8eb3c58a5d
ragilmuhamadsaputra <agungdarusman04@gmail.com>
2026-04-09 07:52:17 +0700

    Add files via upload

---
+++ try4_oop.py
@@ -0,0 +1,15 @@
+a=10
+b=3
+
+print("x=a+b")
+print("x=",a+b)
+print("y=a-b")
+print("y=",a-b)
+print("c=a*b")
+print("c=",a*b)
+print("d=a/b")
+print("d=",a/b)
+print("e=a%b")
+print("e=",a%b)
+print("f=a**b")
+print("f=",a**b)
