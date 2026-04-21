ff0af6d2cef36e930b7ef40c4f9204c5dfb81d02
ragilmuhamadsaputra <agungdarusman04@gmail.com>
2026-04-21 11:53:50 +0700

    Add files via upload

---
+++ try9_oop.py
@@ -0,0 +1,8 @@
+a=20
+b=7
+print("a & b =", a & b, bin(a & b))
+print("a | b =", a | b, bin(a | b))
+print("a ^ b =", a ^ b, bin(a ^ b))
+print("~a =", ~a, bin(~a))
+print("a << 1 =", a << 1, bin(a << 1))
+print("a >> 1 =", a >> 1, bin(a >> 1))
