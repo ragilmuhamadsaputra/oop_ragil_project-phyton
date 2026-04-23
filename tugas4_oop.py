76834533ca921dfda449c382392c6efbbe2d8384
ragilmuhamadsaputra <agungdarusman04@gmail.com>
2026-04-23 09:02:58 +0700

    Add files via upload

---
+++ tugas4_oop.py
@@ -0,0 +1,15 @@
+
+nama=input("masukan nama:")
+print(nama)
+
+umur=input("masukan umur:")
+print("umur saya",umur)
+
+no_hp=input("nomor saya hp:")
+print(no_hp)
+
+email=input("email saya:")
+print(email)
+
+alamat=input("alamat saya:")
+print(alamat)
